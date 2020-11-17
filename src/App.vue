cfcfb4df1d57df2dff67e59836ffc2c834ebf4aa
836050837@qq.com <836050837@qq.com>
2020-11-17 18:38:47 +0800

    初始化项目--仿饿了么

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>项目已启动</p>
+    <p>
+      <i class="make-color"></i>
+    </p>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.make-color {
+  color: red;
+}
+</style>
